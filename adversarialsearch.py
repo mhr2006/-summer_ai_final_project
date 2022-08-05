29373a5aea70f8790b89a57d0bcbccc23f75dad2
Hussain Rashid <hussainrashid@hrs-macbook-air.devices.brown.edu>
2022-08-04 20:59:33 -0400

    final project

--- adversarialsearch.py
+++ adversarialsearch.py
@@ -17,14 +17,75 @@ def minimax(asp: AdversarialSearchProblem[GameState, Action]) -> Action:
     Output:
         an action (an element of asp.get_available_actions(asp.get_start_state()))
     """
-    ...
+    start_state = asp.get_start_state()
+    max_eval = float("-inf") 
+    best_move = None
+    player = start_state.player_to_move()
+    for child in asp.get_available_actions(start_state):
+        next_state = asp.transition(start_state, child)
+        current_eval = helpmin(asp, next_state, player)
+        if current_eval > max_eval:
+            max_eval = current_eval
+            best_move = child
+    assert best_move is not None
+    return best_move
 
+
+def helpmax(asp, state, player):
+    if asp.is_terminal_state(state):
+        return    asp.evaluate_terminal(state)[player]
+    cr_eval = float("-inf")
+    for children in asp.get_available_actions(state):
+        next_state = asp.transition(state, children)
+        cr_eval = max(cr_eval, helpmin(asp, next_state, player))
+
+    return cr_eval
+
+
+def helpmin(asp, state, player):
+    if asp.is_terminal_state(state):
+        return asp.evaluate_terminal(state)[player]
+    cr_eval = float("inf")
+    for children in asp.get_available_actions(state):
+        next_state = asp.transition(state, children)
+        cr_eval = min(cr_eval, helpmax(asp, next_state, player))
+
+    return cr_eval
+
+    
+#See AdversarialSearchProblem:heuristic_func
+#heuristic_func: Callable[[GameState], float] ]-> Action:
+    """
+    This function should:
+    - search through the asp using alpha-beta pruning
+    - cut off the search after cutoff_ply moves have been made.
+
+    Input:
+        asp - an AdversarialSearchProblem
+        cutoff_ply - an Integer that determines when to cutoff the search and
+            use heuristic_func. For example, when cutoff_ply = 1, use
+            heuristic_func to evaluate states that result from your first move.
+            When cutoff_ply = 2, use heuristic_func to evaluate states that
+            result from your opponent's first move. When cutoff_ply = 3 use
+            heuristic_func to evaluate the states that result from your second
+            move. You may assume that cutoff_ply > 0.
+        heuristic_func - a function that takes in a GameState and outputs a
+            real number indicating how good that state is for the player who is
+            using alpha_beta_cutoff to choose their action. You do not need to
+            implement this function, as it should be provided by whomever is
+            calling alpha_beta_cutoff, however you are welcome to write
+            evaluation functions to test your implemention. The heuristic_func
+            we provide does not handle terminal states, so evaluate terminal
+            states the same way you evaluated them in the previous algorithms.
+    Output:
+        an action(an element of asp.get_available_actions(asp.get_start_state()))
+    """
+    
 
 def alpha_beta(asp: AdversarialSearchProblem[GameState, Action]) -> Action:
     """
     Implement the alpha-beta pruning algorithm on ASPs,
     assuming that the given game is both 2-player and constant-sum.
-
     Input:
         asp - an AdversarialSearchProblem
     Output:
@@ -43,7 +104,6 @@ def alpha_beta_cutoff(
     This function should:
     - search through the asp using alpha-beta pruning
     - cut off the search after cutoff_ply moves have been made.
-
     Input:
         asp - an AdversarialSearchProblem
         cutoff_ply - an Integer that determines when to cutoff the search and
